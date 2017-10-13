33a532ca2f9a6a991bd3800fe14a4a1758e55a2c
Steven <sgt.nosky@gmail.com>
2017-10-13 14:27:23 +0200

    fa.h modifié

--- fa.h
+++ fa.h
@@ -34,5 +34,7 @@ void fa_destroy(struct fa *self);
 void fa_set_state_initial(struct fa *self, size_t state);
 void fa_set_state_final(struct fa *self, size_t state);
 
+void fa_add_transition(struct fa *self, size_t from, char alpha, size_t to);
+
 
 #endif //FA_H
