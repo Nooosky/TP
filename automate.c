8d4bca5975f58404caa3cd58e60d2b25f9bf2758
thib39000 <thibaut.bouchard@edu.univ-fcomte.fr>
2017-11-17 09:47:57 +0100

    Ajout des fichiers modifies

--- automate.c
+++ automate.c
@@ -1,9 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "fa.h"
 
 
 int main(int argc, char **argv){
 
+	struct fa *automate = malloc(sizeof(struct fa));
+
+	fa_create(automate,2,5);
+	fa_set_state_initial(automate,0);
+	fa_set_state_initial(automate,1);
+	fa_set_state_final(automate,0);
+	fa_set_state_final(automate,1);
+
+	fa_add_transition(automate,0,'a',1);
+	fa_add_transition(automate,0,'a',2);
+	fa_add_transition(automate,0,'a',3);
+	fa_add_transition(automate,1,'b',3);
+	fa_add_transition(automate,2,'a',3);
+	fa_add_transition(automate,2,'b',4);
+	fa_add_transition(automate,3,'b',3);
+	fa_add_transition(automate,3,'b',4);
+	fa_add_transition(automate,4,'b',4);
+
+	fa_pretty_print(automate,stdout);
 
 
 
